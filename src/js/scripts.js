da4517b2a63b4b4b23ec69584b09b0f692008491
Eduards Egle <eduards@egle.me>
2017-03-18 18:04:20 +0200

    Move data to hard drives.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,9 +2,31 @@
 
   'use strict';
 
+
+  function drawRaid_0() {
+    var data_array = $(".main .data")
+    var data_items = $(".main .data").find(".data-item")
+
+    for (var i = 0; i < data_items.length; i++) {
+      var single_data = data_items[i];
+      // $(single_data).css({
+      //   'position': 'absolute',
+      //   'top': '300px'
+      // })
+      $(single_data).appendTo($('.hard-drive')).show('slow');
+    }
+  }
+
+
+  var startAnimation = function () {
+    drawRaid_0()
+  }
+
   $(function () {
-    // FireShell
-    console.log(232)
+    $('.animation.button').click(function() {
+      startAnimation()
+    })
+
   });
 
 })(jQuery, window, document);
